2239ac24f6360e6deb8dc477a94d8b7995cf219d
farahamawi <farah.amawi@arabiaweather.com>
2017-09-08 20:50:43 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: './src/js/layout.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/assets/dist/bundle.js'
+    },
+    module: {
+        loaders: [{
+            test : /\.jsx?/,
+            exclude: /node_modules/,
+            loader : 'babel-loader',
+        }],
+    }
+}
